32f21a64e748b7f967c41882658c94d070e6951f
lizzard <jiri.kaipr@gmail.com>
2015-08-21 16:17:13 +0200

    adding Rails module to engine

--- lib/bootbox_crud.rb
+++ lib/bootbox_crud.rb
@@ -1,8 +1,8 @@
 module BootboxCrud
   module Rails
-    require "bootbox_crud/version"
-    require "bootbox_crud/engine"
-	require 'bootbox_crud/action_view/helpers'
-	require 'bootbox_crud/railtie' if defined?(Rails)
+    require 'bootbox_crud/version'
+    require 'bootbox_crud/engine'
+    require 'bootbox_crud/action_view/helpers'
+    require 'bootbox_crud/railtie' if defined?(Rails)
   end
 end
