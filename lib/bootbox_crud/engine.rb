f4afe029993c18c241f43847affa8bece6d6c704
lizzard <jiri.kaipr@gmail.com>
2015-08-25 15:05:46 +0200

    added simple_form require to gem engine

--- lib/bootbox_crud/engine.rb
+++ lib/bootbox_crud/engine.rb
@@ -3,6 +3,7 @@ module BootboxCrud
     class Engine < ::Rails::Engine
       require 'bootbox-rails'
       require 'haml'
+      require 'simple_form'
 
       config.app_generators do |g|
         g.templates.unshift File::expand_path('../../templates', __FILE__)
