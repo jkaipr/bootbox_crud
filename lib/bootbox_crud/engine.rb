ef6fb6c4ddf5f352446748f9b4c1e425ed296714
lizzard <jiri.kaipr@gmail.com>
2015-08-25 15:00:01 +0200

    adding template path of generators to Engine definition, fixes haml and controller scaffold

--- lib/bootbox_crud/engine.rb
+++ lib/bootbox_crud/engine.rb
@@ -3,6 +3,10 @@ module BootboxCrud
     class Engine < ::Rails::Engine
       require 'bootbox-rails'
       require 'haml'
+
+      config.app_generators do |g|
+        g.templates.unshift File::expand_path('../../templates', __FILE__)
+      end 
     end
   end
 end
