6efe3fffee44d374209aff049a08f0e421d90603
lizzard <jiri.kaipr@gmail.com>
2015-08-26 19:49:19 +0200

    fixing install generator

--- lib/generators/bootbox_crud/install_generator.rb
+++ lib/generators/bootbox_crud/install_generator.rb
@@ -1,20 +1,17 @@
-class BootboxCrud::InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::ResourceHelpers
-      desc "Copies BootboxCrud default files"
-      source_root File.expand_path('../templates', __FILE__)
+class BootboxCrud::InstallGenerator < ::Rails::Generators::Base
+  desc "Copies BootboxCrud default files"
+  source_root File.expand_path('../templates', __FILE__)
 
-      def copy_config
-        template "config/initializers/simple_form.rb"
-        template "config/initializers/simple_form_bootstrap.rb"
-	    if Rails.application.assets.find_asset('models.js').blank?
-	      copy_file "models.js", "app/assets/javascripts/models.js"
-	    end
-      end
+  def copy_config
+    template "config/initializers/simple_form.rb"
+    template "config/initializers/simple_form_bootstrap.rb"
+    template "app/assets/javascripts/models.js"
+  end
 
-      def show_readme
-        if behavior == :invoke
-          readme "README"
-        end
-      end
+  def show_readme
+    if behavior == :invoke
+      readme "README"
     end
+  end
+end
 end
