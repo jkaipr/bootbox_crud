c532060f242b68456c576c3fe7d1d414edf265f9
lizzard <jiri.kaipr@gmail.com>
2016-03-28 14:14:00 +0200

    added dummy app for additional tests and as example

--- Gemfile
+++ Gemfile
@@ -2,4 +2,10 @@ source 'https://rubygems.org'
 
 gemspec
 
-gem 'codeclimate-test-reporter', group: :test, require: nil
+gem 'rails', '~> 4.2.5'
+gem 'sqlite3', '~> 1.3'
+
+group :development, :test do
+  gem 'codeclimate-test-reporter', require: nil
+  gem 'jasmine', '~> 2.4'
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,11 +3,28 @@ ENV['RAILS_ENV'] = 'test'
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
-require 'rails'
+require File.expand_path("../../test/dummy/config/environment.rb",  __FILE__)
+ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
+
+#require 'rails'
 require 'rails/test_help'
 require 'rails/generators/base'
 require 'rails/generators/test_case'
 require 'generators/generator_test_base'
+
+# Filter out Minitest backtrace while allowing backtrace from other libraries
+# to be shown.
+Minitest.backtrace_filter = Minitest::BacktraceFilter.new
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+# Load fixtures from the engine
+if ActiveSupport::TestCase.respond_to?(:fixture_path=)
+  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
+  ActiveSupport::TestCase.fixtures :all
+end
 
 if ActiveSupport.respond_to?(:test_order)
   ActiveSupport.test_order = :random
