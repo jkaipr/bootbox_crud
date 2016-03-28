4a08210e0492d517902d59ea503c80026b76be70
lizzard <jiri.kaipr@gmail.com>
2016-03-28 14:26:46 +0200

    executed bootbox_crud installer: "bundle exec rails g bootbox_crud:install"

---
+++ test/dummy/app/assets/javascripts/models.js
@@ -0,0 +1,7 @@
+// Put custom actions below this line
+// e.g. BBCrud.Models.addAction('Event', '/events/', 'event', 'close');
+
+
+//!!! Generator adds after this line, do not delete it !!!
+// e.g. BBCrud.Models.add('Customer', '/customers/', 'customer');
+
